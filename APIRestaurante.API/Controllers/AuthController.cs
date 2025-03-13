e4aff4ad76cb7c4b7efdd8bd5c8d63ab51c07471
Renato Silva <dev.renatosilva@gmail.com>
2025-03-12 23:22:37 -0300

    feat(auth): adicionando autenticação a rota de registro

--- APIRestaurante.API/Controllers/AuthController.cs
+++ APIRestaurante.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using APIRestaurante.Application.DTOs;
 using APIRestaurante.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APIRestaurante.API.Controllers
@@ -15,6 +16,7 @@ namespace APIRestaurante.API.Controllers
             _authService = authService;
         }
 
+        [Authorize] 
         [HttpPost("Register")]
         public async Task<IActionResult> Register([FromBody] RegisterDTO registerDto)
         {
